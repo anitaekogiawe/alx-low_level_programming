241463af5f33c74048f893f5ae847c16edd76529
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-08 15:42:44 -0700

    hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	fprintf("Size of a char: %zu byte(s)\n", sizeof(a));
-	fprintf("Size of a int: %zu byte(s)\n", sizeof(b));
-	fprintf("Size of a long int: %zu byte(s)\n", sizeof(c));
-	fprintf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	fprintf("Size of float: %zu byte(s)\n", sizeof(e));
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of a int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of float: %zu byte(s)\n", sizeof(e));
 	return (0);
 }
