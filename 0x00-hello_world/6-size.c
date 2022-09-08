97569c971131b379d60b59f4d3c0bad8ca30571e
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-08 14:30:50 -0700

    a C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print the sizeof var types
+ *
+ * Return: Alwasys 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of a int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
