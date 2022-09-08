2850d59e663a9453d7efe25715748487ab13e18e
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-08 13:54:59 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - Printing
  *
- * Return : Always 0 (sucessful)
- **/
-
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
