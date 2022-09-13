d1ede724a9578caa0a0efac20d64fcf7c1c58f91
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-13 14:02:19 -0700

    fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @b: An integer
+ *
+ * Return: Always 0
+ */
+int _abs(int b)
+{
+	int b;
+
+	if (((b - 0) == -b) || ((b + 0) == b))
+		_putchar(b);
+	return (0);
+}
