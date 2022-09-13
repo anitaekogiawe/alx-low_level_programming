42a0bb8b62096033278f8628f72e9c573c2328bd
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-13 15:00:56 -0700

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,18 @@
 */
 int print_last_digit(int n)
 {
-	_putchar((n % 10) * '1');
-	return (n % 10);
+	int r;
+
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
 }
