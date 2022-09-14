8741b72c31f09351f3c6dd9c9d1e75c638ba8c0d
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-14 06:53:04 -0700

    fix

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - sum multiple of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int start_num, end_num, total;
+
+	end_num = 1024;
+	total = 0;
+
+	for (start_num = 0; start_num < end_num; start_num++)
+	{
+		if ((start_num % 3 == 0) || (start_num % 5 == 0))
+		{
+			total = total + start_num;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d", total);
+	printf("\n");
+	return (0);
+}
