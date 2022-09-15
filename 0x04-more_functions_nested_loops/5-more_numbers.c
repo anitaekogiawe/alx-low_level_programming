70004d1c82369f4502ca9ee43916ee5baa2998bf
“anitaekogiawe” <ekogiaweanita@gmail.com>
2022-09-15 10:19:41 -0700

    hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,32 +2,29 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
  *
  * Return: 0
  */
 void more_numbers(void)
 {
-	int b, c;
+	char n, c;
+	int i = 0;
 
-	b = 0;
-	while (b <= 10)
+	while (i <= 10)
 	{
-		c = 0;
-		while (c <= 14)
+		for (n = 0; n <= 14; n++)
 		{
-			if (c <= 9)
+			c = n;
+			if (c > 9)
 			{
+				_putchar(1);
+				c = n % 10;
+			}
 				_putchar('0' + c);
-			}
-			else
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			c++;
 		}
 		_putchar('\n');
-		b++;
+		i++;
 	}
 }
